4cfce2acbc7a7b3526b80177f33eea75fd239e66
Migue Guillamón <migueguillamon@gmail.com>
2014-03-07 10:47:42 +0100

    1. Introducción al DOM

---
+++ introduccion_al_dom/intro/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('div').fadeOut(1000);
+    $('#verde').fadeOut(1000);
+    $('#nolisto').fadeOut(1000);
+    $('div').slideDown('slow');
+    var parrafo = $('p');
+});
