3ccd0458afc3b9476e7fe5c8ce4abe5370c7c7ad
Migue Guillamón <migueguillamon@gmail.com>
2014-03-10 10:40:25 +0100

    04 - Repaso eventos

--- 04_repaso_eventos/script.js
+++
@@ -1,8 +0,0 @@
-/* EJERCICIO 1 */
-$(document).ready(function() {
-    $('button').click(function() {
-    	var toAdd = $("input[name=message]").val();
-        $('#messages').append("<p>"+toAdd+"</p>");
-    });
-});
-
